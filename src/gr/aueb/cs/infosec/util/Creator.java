ca638e8a0c54a40f55b3ff82442fcb8564edf221
vvalvis <v.valvis@hotmail.gr>
2016-04-10 16:59:32 +0300

    Utility changes to NodeCreator and Creator.
    Leaning towards a more modular implementation

--- src/gr/aueb/cs/infosec/util/Creator.java
+++ src/gr/aueb/cs/infosec/util/Creator.java
@@ -6,6 +6,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public abstract class Creator {
 
@@ -13,10 +16,12 @@ public abstract class Creator {
   private String output;
   private BufferedReader in;
   private BufferedWriter out;
+  private Map<String, String> node_pairs;
 
   public Creator(String input, String output) {
     this.input = input;
     this.output = output;
+    this.node_pairs = new HashMap<String, String>();
     this.initialize();
   }
 
@@ -41,6 +46,8 @@ public abstract class Creator {
     } catch (IOException io) {
       io.printStackTrace();
     }
+    this.node_pairs.clear();
+    this.node_pairs = null;
   }
 
   public abstract void process();
@@ -59,5 +66,21 @@ public abstract class Creator {
 
   public BufferedWriter getWriter() {
     return this.out;
+  }
+
+  public Map<String, String> getNodePairs() {
+    return this.node_pairs;
+  }
+
+  public String[] splitNodeNames(String inputLine) {
+    String[] results = new String[3];
+    StringTokenizer tokenizer =
+        new StringTokenizer((inputLine.split(",")[1]).split("between")[1], "and,(");
+    results[0] = tokenizer.nextToken().replaceAll("^\\s+|\\s+$", "");
+    String first = (inputLine.split(",")[1]).split(" ")[0].replaceAll("^\\s+|\\s+$", "");
+    String second = tokenizer.nextToken().replaceAll("^\\s+|\\s+$", "");
+    results[1] = first + "-" + second;
+    results[2] = inputLine.split(",")[0];
+    return results;
   }
 }
--- src/gr/aueb/cs/infosec/util/NodeCreator.java
+++ src/gr/aueb/cs/infosec/util/NodeCreator.java
@@ -2,27 +2,15 @@ package gr.aueb.cs.infosec.util;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
 
 public class NodeCreator extends Creator {
 
   // this could also be in the parent class
   private final String CSV_HEADER = "node_name:ID,connected_node,:LABEL";
   private final String LABEL = "Traffic node";
-  private Map<String, String> node_pairs;
 
   public NodeCreator(String input, String output) {
     super(input, output);
-    this.node_pairs = new HashMap<String, String>();
-  }
-
-  @Override
-  public void finalize() {
-    super.finalize();
-    this.node_pairs.clear();
-    this.node_pairs = null;
   }
 
   @Override
@@ -40,17 +28,14 @@ public class NodeCreator extends Creator {
       out.write("\n");
       while ((nextLine = in.readLine()) != null) {
         try {
-          StringTokenizer tokenizer =
-              new StringTokenizer((nextLine.split(",")[1]).split("between")[1], "and,(");
-          String node_name = tokenizer.nextToken().replaceAll("^\\s+|\\s+$", "");
-          String first = (nextLine.split(",")[1]).split(" ")[0].replaceAll("^\\s+|\\s+$", "");
-          String second = tokenizer.nextToken().replaceAll("^\\s+|\\s+$", "");
-          String node_pair = first + "-" + second;
-          if (this.node_pairs.get(node_name) != null) {
+          String[] split_nodes = this.splitNodeNames(nextLine);
+          String node_name = split_nodes[0];
+          String node_pair = split_nodes[1];
+          if (this.getNodePairs().get(node_name) != null) {
             // we don't want to write duplicate entries
             continue;
           }
-          this.node_pairs.put(node_name, node_pair);
+          this.getNodePairs().put(node_name, node_pair);
           out.write(node_name);
           out.write(",");
           out.write(node_pair);
